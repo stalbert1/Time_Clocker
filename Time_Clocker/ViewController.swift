de2156958eb6f847225e80a71867f1dede85ba9c
Shane Talbert <stalbert1@tampabay.rr.com>
2018-04-23 15:07:41 -0400

    Initial Commit

---
+++ Time_Clocker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time_Clocker
+//
+//  Created by Shane Talbert on 4/23/18.
+//  Copyright © 2018 Shane Talbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
